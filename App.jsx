8bf3b89687774fc8fd8bf069e9c56ddcc5385228
CSuperman <crlbusiness1@gmail.com>
2023-11-20 22:00:46 -0500

    Create App.jsx
    setup

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import styles from "styled-components"
+
+const div = styles.div'
+  margin: 0.5rem 1rem;
+  padding: 0.5rem 0;
+  opacity: 0.7;
+'
+
+const App = () => {
+	return <div> This is limited Christmas page for December </div>
+}	
+
+export default App;
