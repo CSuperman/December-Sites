dddb026fba6668b07e4f89da24738724e634ea97
CSuperman <crlbusiness1@gmail.com>
2023-11-21 20:46:18 -0500

    Create Navbar.jsx

---
+++ Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from 'styled-components'
+
+const div = styleds.div'
+	height: 75px;
+'
+
+const NavBar = () => {
+	return (
+		<div>
+			link
+		</div>
+	)
+}
+
+export default Navbar
