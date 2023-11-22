c777d4de44cb0f2a86401988671d23be04e6d452
CSuperman <crlbusiness1@gmail.com>
2023-11-21 20:47:14 -0500

    Update Navbar.jsx
    updates

--- Navbar.jsx
+++ Navbar.jsx
@@ -5,10 +5,20 @@ const div = styleds.div'
 	height: 75px;
 '
 
+const packager = styles.div'
+	padding 20px 35px;
+	display: flex;
+	justify-content: space-between;
+'
+
 const NavBar = () => {
 	return (
 		<div>
-			link
+			<packager>
+				<span> logo </span>
+				<span> about </span>
+				<span> contact </span>
+			</packager>
 		</div>
 	)
 }
