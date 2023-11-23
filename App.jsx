d61d90b3d7feb74dced993ab07d9f4f5ecad4eaf
CSuperman <crlbusiness1@gmail.com>
2023-11-22 21:58:52 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,13 +1,21 @@
-import styles from "styled-components"
+import styled from "styled-components"
+import Navbar from "./Navbar"
+import 1st from "./1st"
 
-const div = styles.div'
+const div = styled.div'
   margin: 0.5rem 1rem;
   padding: 0.5rem 0;
   opacity: 0.7;
+
+
+  &:hover {
+    filter: brightness(0.85);
+  }
+
 '
 
 const App = () => {
-	return <div> This is limited Christmas page for December </div>
+	return <div> <Navbar/> <1st/> </div>
 }	
 
 export default App;
