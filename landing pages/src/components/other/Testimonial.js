f23422e09f32de31e3ecebf304a31667509289e1
LAPTOP-KD1K6DO5\Clark <crlbusiness1@gmail.com>
2023-12-03 22:50:57 -0500

    landing page development

---
+++ landing pages/src/components/other/Testimonial.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import usera from ''
+import userb from ''
+
+
+
+const Testimonial = () => {
+    return (
+        <React.Fragment>
+            <section className="content-container">
+                <div className="textArea"> 
+                    <h2>Testimonials</h2>
+                    <p>What others say about us.</p>
+                </div>
+                <div className="testimonial-container">
+                    <img src={usera} alt="Avatar" style={{width:"75px"}}/>
+                    <p><span>Bob Smith</span> Pizza boy</p>
+                    <p>Perfecto!</p>
+                </div>
+
+                <div className="testimonial-container">
+                    <img src={userb} alt="" style={{width:"75px"}}/>
+                    <p><span>Karen</span> Teacher</p>
+                    <p> Will buy again.</p>
+                </div>
+
+                <div className="testimonial-container">
+                    <img src={userc} alt="" style={{width:"75px"}}/>
+                    <p><span>Influencer</span> Influencer</p>
+                    <p>Best of the best.</p>
+                </div>
+            </section>
+        </React.Fragment>
+    )
+}
+
+export default Testimonial;
