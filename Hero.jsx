ab30b29b86dedd3e54d791cc3d9da25bf3b131b6
CSuperman <crlbusiness1@gmail.com>
2023-12-04 22:51:32 -0500

    Update Hero.jsx

--- Hero.jsx
+++ Hero.jsx
@@ -1,1 +1,12 @@
- <span> <h2> Welcome to a place of christmas spirit and holiday joy. </h2> </span>
+import React from 'react'
+
+function Hero() {
+  return (
+   <div>
+    <span> 
+     <h2> Welcome to a place of christmas spirit and holiday joy. </h2> 
+    </span>   
+   </div>
+  )
+}
+export default Hero;
